878b0fef2cb1e2e22211ef5ea7a16a0d191fca4c
Hamin Lee <whereanna7@gmail.com>
2023-12-14 00:07:08 +0900

    [ADD] globalstyle add

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,11 @@
 import React from "react";
-import { createGlobalStyle } from "styled-components";
-import reset from "styled-reset";
 
 function App() {
   return (
-    <GlobalStyle>
-      <h1>aaa</h1>
-    </GlobalStyle>
+    <>
+      <h1>Todo List</h1>
+    </>
   );
 }
 
 export default App;
-
-const GlobalStyle = createGlobalStyle`
-${reset}
-*{
-  box-sizing: border-box;
-}`;
--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
+const GlobalStyle = createGlobalStyle`
+${reset}
+*{
+  box-sizing: border-box;
+}`;
+
 root.render(
-  <React.StrictMode>
+  <>
+    <GlobalStyle />
     <App />
-  </React.StrictMode>
+  </>
 );
 
 // If you want to start measuring performance in your app, pass a function
